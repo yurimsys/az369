9aa06df410621d73acb808fa387cf24d4897dace
kudy96 <kudy96@naver.com>
2020-02-19 19:26:50 +0900

    회원가입 중복확인 및 가입완료

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'qw12',
-    database : 'BusReservation' 
+    database : 'BusReservation'
 });
 
 connection.config.queryFormat = function (query, values) {
@@ -22,7 +22,7 @@ connection.config.queryFormat = function (query, values) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AZ369' });
 });
 
 router.get('/mypage', function(req, res, next) {
@@ -48,6 +48,8 @@ router.get('/reservation', (req, res) => {
     res.render('reservation_01');
 });
 
+
+
 router.post('/api/user/phone', (req, res, next) => {
   
     let query  = "update tU set u_phone = :newPhone where u_name = :name";
@@ -71,4 +73,80 @@ router.post('/api/user/phone', (req, res, next) => {
     // 로그 추가
 });
 
+router.get('/benefit_application', function(req, res, next){
+    res.render('benefit_application', { title : '입점신청' });
+});
+
+router.get('/login', function(req, res, next){
+    res.render('login', { title : '로그인' });
+});
+
+router.get('/findId1', function(req, res, next) {
+    res.render('find_id_01');
+  });
+
+router.get('/findId2', function(req, res, next) {
+    res.render('find_id_02');
+});
+
+
+  //회원가입 페이지
+router.get('/join2', function(req, res, next) {
+    res.render('join_02');
+});
+
+//회원가입 액션
+router.post('/join/user', (req, res, next) =>{
+    let query = `insert into tu (U_UserName, U_Pw, U_Name, U_Phone, U_Brand, U_Zip, U_Addr1, U_Addr2) 
+            values( :uUserName,  :uPw,  :uName,  :uPhone , :uBrand,  :uZip,  :uAddr1,  :uAddr2)`;
+    //console.log(req.body);
+
+    connection.query(query, 
+        {
+            uUserName : req.body.id,
+            uPw : req.body.password,
+            uName : req.body.name,
+            uPhone : req.body.phone,
+            uBrand : req.body.brand,
+            uZip : req.body.postcode,
+            uAddr1 : req.body.address,
+            uAddr2 : req.body.detailAddress           
+        },
+        function(err, rows, fields) {
+            if (err) throw err;
+            res.json({
+                statusCode : 200
+            });
+        });
+});
+
+//아이디 중복확인
+router.post('/join/overlap', (req, res, next) =>{
+    let query = "select U_UserName from tu where U_UserName = :uUserName";
+    
+    console.log(req.body);
+
+    connection.query(query, 
+        {
+            uUserName : req.body.id                      
+        },
+        function(err, rows, fields) {
+            if (err) throw err;
+            // if(rows.length == 0){
+            //     check = 1;
+            // }else{
+            //     check = 0;
+            // }
+            //console.log(rows);
+            // res.json({ data : check })
+            //console.log(check);
+            res.json( {  data : rows[0] });
+        });
+        
+});
+
+
+router.get('/join3', function(req, res, next) {
+    res.render('join_03');
+});
 module.exports = router;
