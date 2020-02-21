998bdaa000c32bc6d584fdbb222785e0da3deee5
aeioukim <aizenhero@gmail.com>
2020-02-22 01:21:39 +0900

    add auth in apiRouter

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const auth = require('../config/passport');
 
 router.post('/user/phone', (req, res, next) => {
   
