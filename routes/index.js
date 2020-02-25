20024e319c4ad30d0c78fbe1568cf296ed05b7ce
kudy96 <kudy96@naver.com>
2020-02-25 09:14:21 +0900

    중간커밋

--- routes/index.js
+++ routes/index.js
@@ -63,7 +63,7 @@ router.get('/mypage',  auth.isLoggedIn, function(req, res, next) {
     let query = `SELECT * FROM tCT 
                   INNER JOIN tCR 
                   ON tCT.CT_ID = tCR.CR_CT_ID WHERE tCR.CR_Cancel = :crCancel AND tCR.CR_U_ID = :sessionId
-                  AND tCT.CT_DepartureTe < now() order by tCT.CT_DepartureTe desc`; //서비스 후 < now() 변경
+                  AND tCT.CT_DepartureTe < now() order by tCT.CT_DepartureTe desc`; //서비스 후 > now() 변경
     connection.query(query, { sessionId, crCancel},
       function(err, rows, fields) {
           if (err) throw err;
