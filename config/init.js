c851af467994341e59000c48dc39bca8b629ad4b
aeioukim <aizenhero@gmail.com>
2020-02-27 11:55:21 +0900

    add test server config

--- config/init.js
+++ config/init.js
@@ -5,6 +5,9 @@ if(process.argv.length > 2){
         process.env.NODE_ENV = "development";
     } else if(env == "stage"){
         process.env.NODE_ENV = "stage";
+    } else if(env == "test"){
+        process.env.NODE_ENV = "development";
+        process.env.PORT = 80;
     }
 } else{
     process.env.NODE_ENV = "production";
